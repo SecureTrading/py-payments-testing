69c16e0e023c19680c24179acdb493bb91a37e06
Marek Sakowicz (PGS Software) <msakowicz@pgs-soft.com>
2020-03-23 09:28:55 +0100

    skip first step also for test with tag defer_init_and_start_on_load_true

--- features/steps/payment_methods_steps.py
+++ features/steps/payment_methods_steps.py
@@ -54,15 +54,15 @@ def step_impl(context):
 @step("User opens page with payment form")
 def step_impl(context):
     payment_page = context.page_factory.get_page(page_name='payment_methods')
-    if 'config_immediate_payment' not in context.scenario.tags:
-        if 'parent_iframe' not in context.scenario.tags:
-            if ('safari' in context.browser) or ('iP' in CONFIGURATION.REMOTE_DEVICE):
-                payment_page.open_page(MockUrl.WEBSERVICES_DOMAIN.value)
-                if 'visa_test' in context.scenario.tags or 'apple_test' in context.scenario.tags:
-                    payment_page.open_page(MockUrl.THIRDPARTY_URL.value)
-            payment_page.open_page(CONFIGURATION.URL.BASE_URL)
-            payment_page.is_connection_not_private_dispayed(CONFIGURATION.URL.BASE_URL)
-            payment_page.wait_for_iframe()
+    if 'config_immediate_payment' not in context.scenario.tags and 'parent_iframe' not in context.scenario.tags and \
+            'config_defer_init_and_start_on_load_true' not in context.scenario.tags:
+        if ('safari' in context.browser) or ('iP' in CONFIGURATION.REMOTE_DEVICE):
+            payment_page.open_page(MockUrl.WEBSERVICES_DOMAIN.value)
+            if 'visa_test' in context.scenario.tags or 'apple_test' in context.scenario.tags:
+                payment_page.open_page(MockUrl.THIRDPARTY_URL.value)
+        payment_page.open_page(CONFIGURATION.URL.BASE_URL)
+        payment_page.is_connection_not_private_dispayed(CONFIGURATION.URL.BASE_URL)
+        payment_page.wait_for_iframe()
 
 
 @when(
