1f81eca0ac37b689142228ee6fa6708b3e3c0f5a
kusiakpgs <kusiakpgs@gmail.com>
2020-07-27 12:11:14 +0200

    remove iss from jsons and change generating it in jwt generator

--- utils/browser.py
+++ utils/browser.py
@@ -1,5 +1,6 @@
 """ This class consist all methods related with browser activities"""
 from configuration import CONFIGURATION
+from utils.enums.jwt_config import JwtConfig
 from utils.waits import Waits
 from utils.configurations.jwt_generator import encode_jwt_for_json
 
@@ -10,7 +11,7 @@ class Browser(Waits):
         self._browser.get(page_url)
         self.fullscreen()
 
-    def open_page_with_jwt_config(self, page_url, jwt_json_config):
+    def open_page_with_jwt_config(self, page_url, jwt_json_config: JwtConfig):
         jwt = encode_jwt_for_json(jwt_json_config)
         self._browser.get(page_url + f'?jwt={jwt}')
         self.fullscreen()
--- utils/configurations/jwt_generator.py
+++ utils/configurations/jwt_generator.py
@@ -7,7 +7,8 @@ from utils.enums.jwt_config import JwtConfig
 
 JWT_JSON_CONFIG_FILES_BESEDIR = os.path.abspath('jwt_config_files')
 
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'you_will_never_guess'
+SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'you_will_never_guess'
+ISS_KEY = os.environ.get('JWT_ISS_KEY') or 'you_will_never_guess'
 
 
 def get_data_from_json(jwt_config):
@@ -18,4 +19,5 @@ def get_data_from_json(jwt_config):
 
 def encode_jwt_for_json(jwt_config: JwtConfig):
     data = get_data_from_json(jwt_config.value)
-    return jwt.encode({"iat": int(time.time()), "iss": data["iss"], "payload": data["payload"]}, SECRET_KEY, algorithm='HS256')
+    print(data)
+    return jwt.encode({"iat": int(time.time()), "iss": ISS_KEY, "payload": data["payload"]}, SECRET_KEY, algorithm='HS256')
