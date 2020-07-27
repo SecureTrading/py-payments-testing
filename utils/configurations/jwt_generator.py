aee1a458e503f638d0b953842bd7989ba9fd709e
kusiakpgs <kusiakpgs@gmail.com>
2020-07-27 10:22:00 +0200

    change type for jwt_generator method to enum value

--- utils/configurations/jwt_generator.py
+++ utils/configurations/jwt_generator.py
@@ -3,9 +3,11 @@ import jwt
 import json
 import os
 
+from utils.enums.jwt_config import JwtConfig
+
 JWT_JSON_CONFIG_FILES_BESEDIR = os.path.abspath('jwt_config_files')
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'you_will_never_guess'
 
 
 def get_data_from_json(jwt_config):
@@ -14,8 +16,6 @@ def get_data_from_json(jwt_config):
     return jwt_json
 
 
-def encode_jwt_for_json(json_config, secret):
-    data = get_data_from_json(json_config)
-    return jwt.encode({"iat": int(time.time()), "iss": data["iss"], "payload": data["payload"]}, secret, algorithm='HS256')
-
-
+def encode_jwt_for_json(jwt_config: JwtConfig):
+    data = get_data_from_json(jwt_config.value)
+    return jwt.encode({"iat": int(time.time()), "iss": data["iss"], "payload": data["payload"]}, SECRET_KEY, algorithm='HS256')
