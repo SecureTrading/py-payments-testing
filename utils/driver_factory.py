d84a48f04304001977241a81682685ab025bb51a
Maciej Rokosz <mrokosz@pgs-soft.com>
2020-09-02 13:12:04 +0200

    Remove executable path from driver_factory

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -102,7 +102,7 @@ class SeleniumDriver(Driver):
         if self._browser_name == "chrome":
             kwargs['chrome_options'] = Options()
             kwargs['chrome_options'].binary_location = "/usr/bin/google-chrome"
-            kwargs['chrome_options'].add_argument('executable_path=/chromedriver.exe')
+            # kwargs['chrome_options'].add_argument('executable_path=/chromedriver')
             kwargs['chrome_options'].headless = True
             kwargs['chrome_options'].add_argument('--no-sandbox')
             kwargs['chrome_options'].add_argument('--disable-dev-shm-usage')
